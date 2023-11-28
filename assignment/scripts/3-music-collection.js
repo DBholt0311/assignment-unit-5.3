829d58c6e93a7b37fd1bef11584c4e69aa62ae2c
Daniel Holt <holt.daniel.b@gmail.com>
2023-11-28 14:37:09 -0500

    fist function finished, can't get second test to pass

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,8 +1,39 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
-
-
-
+  
+let myCollection = [];
+  
+const addToCollection = function (collection, title, artist, yearPublished) {
+  let record = {
+    collection: collection,
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
+  };
+  myCollection.push(record);
+}
+addToCollection('rap', 'Man on the Moon', 'Kid Cudi', 2009);
+addToCollection('electronic', 'Discovery', 'Daft Punk', 2001);
+addToCollection('rock', `Good Apollo I'm Burning Star IV`, 'Coheed and Cambria', 2005);
+addToCollection('indie', 'The Ocean Went Mad and We Were to Blame', 'The Dead South', 2013);
+addToCollection('electronic', 'Gorillaz', 'Gorillaz', 2001);
+addToCollection('rock', 'Hybrid Theory', 'Linkin Park', 2000);
+console.log(myCollection);
+    //- Add the new object to the end of the `collection` array.
+    //- `return` the newly created object.
+    const showCollection = function (collection) {
+      console.log(collection);
+    }
+showCollection(myCollection);
+  //create a function named showCollection
+    //- Take in a `collection` parameter. (This allows it to be reused to show any array of album objects.)
+    //- Loop through the `collection` and `console.log` each album's information formatted **within a single string**, like: `TITLE by ARTIST, published in YEAR`.
+  //create a function named findByArtist
+    //- Take in a `collection` parameter. Remember, we want to be able to search any collection!
+    //- Take in an `artist` (string) parameter.
+    //- Create an empty array to hold any matching results, if any.
+    //- Loop through the `collection` and add any album objects with a matching artist to the array.
+    //- Return the array with the matching results. (If no results are found, an empty array should be returned.)  
 
 
 
